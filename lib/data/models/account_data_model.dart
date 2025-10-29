6f31c76fa39cae3fc4f2274af19ad1e80e8f790c
carloscallejas <carloscallejas91@outlook.com>
2025-10-29 11:09:22 -0300

    refactor: Simplifica construtor e ajusta importação do `AccountDataModel`

--- lib/data/models/account_data_model.dart
+++ lib/data/models/account_data_model.dart
@@ -1,21 +1,17 @@
-import 'package:bytebank_app/domain/entities/account_entity.dart';
+import 'package:mobile_app/domain/entities/account_entity.dart';
 
 class AccountDataModel extends AccountEntity {
   AccountDataModel({
-    required String last4Digits,
-    required String validity,
-    required String accountType,
-  }) : super(
-          last4Digits: last4Digits,
-          validity: validity,
-          accountType: accountType,
-        );
+    required super.last4Digits,
+    required super.validity,
+    required super.accountType,
+  });
 
   factory AccountDataModel.fromMap(Map<String, dynamic> data) {
     return AccountDataModel(
       last4Digits: data['account_last4'] ?? '0000',
       validity: data['account_validity'] ?? '00/00',
-      accountType: data['account_type'] ?? 'Conta Corrente',
+      accountType: data['account_type'] ?? 'indefinido',
     );
   }
 }
