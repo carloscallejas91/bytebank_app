26af8a79c5577f7cabb7d9402d1a39d79c8e1f8d
carloscallejas <carloscallejas91@outlook.com>
2025-10-30 17:20:00 -0300

    refactor: Centraliza lógica de exibição de Snackbars
    Este commit refatora o `SnackBarService` para eliminar a duplicação de código, introduzindo um método privado `_showSnackBar`.

--- lib/app/services/snack_bar_service.dart
+++ lib/app/services/snack_bar_service.dart
@@ -3,39 +3,50 @@ import 'package:get/get.dart';
 import 'package:mobile_app/app/ui/theme/app_color_extensions.dart';
 
 class SnackBarService extends GetxService {
-  void showError({required String title, String? message}) {
-    final theme = Theme.of(Get.context!);
+  ThemeData get _theme => Theme.of(Get.context!);
 
+  AppColorExtensions get _customColors =>
+      _theme.extension<AppColorExtensions>()!;
+
+  void _showSnackBar({
+    required String title,
+    required String message,
+    required Color backgroundColor,
+    required Color colorText,
+  }) {
     Get.snackbar(
       title,
-      message ?? 'Ocorreu um erro, tente novamente mais tarde.',
+      message,
       snackPosition: SnackPosition.TOP,
-      backgroundColor: theme.colorScheme.error,
-      colorText: theme.colorScheme.onError,
+      backgroundColor: backgroundColor,
+      colorText: colorText,
+    );
+  }
+
+  void showError({required String title, String? message}) {
+    _showSnackBar(
+      title: title,
+      message: message ?? 'Ocorreu um erro, tente novamente mais tarde.',
+      backgroundColor: _theme.colorScheme.error,
+      colorText: _theme.colorScheme.onError,
     );
   }
 
   void showSuccess({required String title, required String message}) {
-    final customColors = Theme.of(Get.context!).extension<AppColorExtensions>()!;
-
-    Get.snackbar(
-      title,
-      message,
-      snackPosition: SnackPosition.TOP,
-      backgroundColor: customColors.success,
-      colorText: customColors.onSuccess,
+    _showSnackBar(
+      title: title,
+      message: message,
+      backgroundColor: _customColors.success!,
+      colorText: _customColors.onSuccess!,
     );
   }
 
   void showWarning({required String title, required String message}) {
-    final customColors = Theme.of(Get.context!).extension<AppColorExtensions>()!;
-
-    Get.snackbar(
-      title,
-      message,
-      snackPosition: SnackPosition.TOP,
-      backgroundColor: customColors.warning,
-      colorText: customColors.onWarning,
+    _showSnackBar(
+      title: title,
+      message: message,
+      backgroundColor: _customColors.warning!,
+      colorText: _customColors.onWarning!,
     );
   }
 }
